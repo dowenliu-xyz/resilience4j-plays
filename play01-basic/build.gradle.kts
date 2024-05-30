e29e3db926cd9c45887f59d45e5577dcb868aa03
dowenliu-xyz <hawkdowen@hotmail.com>
2024-05-30 14:34:40 +0800

    remove useless file; update build script

--- play01-basic/build.gradle.kts
+++ play01-basic/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-aop")
